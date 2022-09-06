a4b765e89ecb94821a785d72c62294efb1b56d7d
Marko van Treeck <mvantreeck@ukaachen.de>
2022-09-06 17:06:10 +0200

    Bugfixes
    - Improved cleanup of temporary files on unclean exit.
    - Fix bug which led to one slide being dropped.

--- tile.py
+++ tile.py
@@ -3,22 +3,23 @@
 __author__ = 'Marko van Treeck'
 __copyright__ = 'Copyright 2022, Kather Lab'
 __license__ = 'MIT'
-__version__ = '0.2.1'
+__version__ = '0.2.2'
 __maintainer__ = ['Marko van Treeck', 'Omar El Nahhas']
 __email__ = 'markovantreeck@gmail.com'
 
 
 '''
 Version 0.2.0 from 29-08-2022, added Canny edge detector to remove blurry/white
-tiles which are not useful. The prior method of filtering everything > 224 pixels
-(i.e., white slides) did not account for blur or black tiles, or mostly white tiles.
-The edge cut-off (>2) has been hard-coded and adapted from the Normalisation script 
-in the old-gen pre-processing script. Note that Canny itself has hard-coded thresholds
-as well (40, 100).
+tiles which are not useful. The prior method of filtering everything > 224
+pixels (i.e., white slides) did not account for blur or black tiles, or mostly
+white tiles.  The edge cut-off (>2) has been hard-coded and adapted from the
+Normalisation script in the old-gen pre-processing script. Note that Canny
+itself has hard-coded thresholds as well (40, 100).
 
-Version 0.2.1 from 30-08-2022 added canny as optional input to run
+Version 0.2.1 from 30-08-2022 added canny as optional input to run.
 '''
 
+from contextlib import contextmanager
 import os
 import shutil
 import tempfile
@@ -31,9 +32,7 @@ from pathlib import Path
 from PIL import Image
 from typing import Tuple
 from common import supported_extensions
-import time
 import logging
-import queue
 import cv2
 
 
@@ -51,37 +50,54 @@ def main(
         force:  Overwrite existing tiles.
     """
     cohort_path, outpath = Path(cohort_path), Path(outpath)
+    outpath.mkdir(exist_ok=True, parents=True)
     logging.basicConfig(filename=outpath/'logfile', level=logging.DEBUG)
     logging.getLogger().addHandler(logging.StreamHandler())
     slides = sum((list(cohort_path.glob(f'**/*.{ext}'))
                   for ext in supported_extensions),
                  start=[])
 
-    tmpdir = tempfile.mkdtemp(prefix='tile')
     submitted_jobs = {}
-    with futures.ThreadPoolExecutor(1) as executor:
+    with (futures.ThreadPoolExecutor(1) as executor,
+          tempdir(prefix='tile-') as tmpdir):
         for i, slide_path in enumerate((progress := tqdm(slides))):
-            try:
-                progress.set_description(slide_path.stem)
-                tmp_slide_path = Path(tmpdir)/slide_path.name
-                shutil.copy(slide_path, tmp_slide_path)
-                future = executor.submit(
-                    extract_tiles,
-                    tmp_slide_path,
-                    outpath/slide_path.relative_to(cohort_path).parent/slide_path.stem,
-                    tile_size=tile_size,
-                    um_per_tile=um_per_tile,
-                    threshold=threshold,
-                    force=force,
-                    canny=canny)
-                submitted_jobs[future] = tmp_slide_path
-                if len(submitted_jobs) > 2:
-                    done, _ = futures.wait(submitted_jobs, return_when=futures.FIRST_COMPLETED)
-                    for future in done:
-                        submitted_jobs[future].unlink()
-                        del submitted_jobs[future]
-            except Exception as e:
-                logging.exception(f'{tmp_slide_path}: {e}')
+            progress.set_description(slide_path.stem)
+            tmp_slide_path = tmpdir/slide_path.name
+            shutil.copy(slide_path, tmp_slide_path)
+
+            future = executor.submit(
+                extract_tiles,
+                slide_path=tmp_slide_path,
+                outdir=outpath /
+                slide_path.relative_to(cohort_path).parent/slide_path.stem,
+                tile_size=tile_size,
+                um_per_tile=um_per_tile,
+                threshold=threshold,
+                force=force,
+                canny=canny)
+            submitted_jobs[future] = tmp_slide_path     # to delete later
+
+            while len(submitted_jobs) > 2 or (submitted_jobs and i == len(slides) - 1):
+                done, _ = futures.wait(
+                    submitted_jobs, return_when=futures.FIRST_COMPLETED)
+                for future in done:
+                    # delete temporary slide copy
+                    submitted_jobs[future].unlink()
+                    try:
+                        future.result()     # force result to get eventual exceptions
+                    except Exception as e:
+                        logging.exception(f'{slide_path}: {e}')
+                    del submitted_jobs[future]
+
+
+@contextmanager
+def tempdir(*args, **kwargs):
+    """A context manager to (hopefully) clean up our tmpdir on a crash."""
+    path = Path(tempfile.mkdtemp(*args, **kwargs))
+    try:
+        yield path
+    finally:
+        shutil.rmtree(path)
 
 
 def extract_tiles(
@@ -94,20 +110,33 @@ def extract_tiles(
 
     tile_size_px, thumb = get_scaled_thumb(slide, um_per_tile)
 
-    #the mask contains the tiles which have survived the threshold criteria of being < 224 pixels
+    # the mask contains the tiles which have survived the threshold criteria of being < 224 pixels
     mask = get_mask_from_thumb(thumb, threshold)
-    
-    #coords has dimension of: amount of tiles * (x,y) coords of tiles
+
+    # coords has dimension of: amount of tiles * (x,y) coords of tiles
     coords = np.flip(np.transpose(mask.nonzero()), 1) * tile_size_px
 
     outdir.mkdir(exist_ok=True, parents=True)
-    with futures.ThreadPoolExecutor(os.cpu_count()) as e:
+    with futures.ThreadPoolExecutor(os.cpu_count()) as executor:
+        jobs = []
         for c in coords:
             c = c.astype(int)
             fn = outdir/f'{outdir.stem}_({c[0]},{c[1]}).jpg'
             if fn.exists() and not force:
                 continue
-            e.submit(read_and_save_tile, slide, fn, c, tile_size_px, tile_size, canny)
+            future = executor.submit(
+                read_and_save_tile,
+                slide=slide, outpath=fn, coords=c,
+                tile_size_px=tile_size_px, tile_size_out=tile_size,
+                use_canny=canny)
+            jobs.append(future)
+
+        for future in tqdm(futures.as_completed(jobs), total=len(jobs), leave=False):
+            try:
+                future.result()
+            except Exception as e:
+                logging.exception(f'{slide_path}: {e}')
+
 
 
 def get_scaled_thumb(slide: OpenSlide, um_per_tile: float) -> Tuple[float, Image.Image]:
@@ -123,40 +152,39 @@ def get_mask_from_thumb(thumb, threshold: int) -> np.ndarray:
     return np.array(thumb) < threshold
 
 
-def read_and_save_tile(slide, outpath, coords, tile_size_px, tile_size_out, canny):
+def read_and_save_tile(*, slide, outpath, coords, tile_size_px, tile_size_out, use_canny):
     tile = slide.read_region(coords, 0, (int(tile_size_px),)*2)
-    
-    #True by default, which runs Canny edge detection
-    if canny:
+
+    # True by default, which runs Canny edge detection
+    if use_canny:
         # Below was added in version 0.2.0, using Canny as extra filtering method
         tile_to_greyscale = tile.convert('L')
-        # tile_to_greyscale is an PIL.Image.Image with image mode L 
-        # Note: If you have an L mode image, that means it is 
-        # a single channel image - normally interpreted as greyscale. 
-        # The L means that is just stores the Luminance. 
+        # tile_to_greyscale is an PIL.Image.Image with image mode L
+        # Note: If you have an L mode image, that means it is
+        # a single channel image - normally interpreted as greyscale.
+        # The L means that is just stores the Luminance.
         # It is very compact, but only stores a greyscale, not colour.
 
         tile2array = np.array(tile_to_greyscale)
 
-        #hardcoded thresholds 40 and 100
+        # hardcoded thresholds
         edge = cv2.Canny(tile2array, 40, 100)
-        
-        #avoid dividing by zero 
-        edge = (edge / np.max(edge) if np.max(edge) != 0 else 0)               
-        edge = (((np.sum(np.sum(edge)) / (tile2array.shape[0]*tile2array.shape[1])) * 100)\
-            if (tile2array.shape[0]*tile2array.shape[1]) != 0 else 0)
-        
+
+        # avoid dividing by zero
+        edge = (edge / np.max(edge) if np.max(edge) != 0 else 0)
+        edge = (((np.sum(np.sum(edge)) / (tile2array.shape[0]*tile2array.shape[1])) * 100)
+                if (tile2array.shape[0]*tile2array.shape[1]) != 0 else 0)
+
         tile = tile.convert('RGB').resize((tile_size_out,)*2)
-        
-        #hardcoded limit. Less or equal to 2 edges will be rejected (i.e., not saved)
+
+        # hardcoded limit. Less or equal to 2 edges will be rejected (i.e., not saved)
         if(edge < 2.):
-            logging.info(f"Tile rejected, found 2 or less edges. Tile: {outpath}")
-        else:
-            tile.save(outpath)
-    else:
-        #if the canny variable is set to False, it will just immediatly save the tile
-        tile = tile.convert('RGB').resize((tile_size_out,)*2)
-        tile.save(outpath)
+            logging.info(
+                f'Tile rejected, found 2 or less edges. Tile: {outpath}')
+        return
+
+    tile = tile.convert('RGB').resize((tile_size_out,)*2)
+    tile.save(outpath)
 
 
 if __name__ == '__main__':
